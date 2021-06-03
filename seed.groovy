3146b7ae8e4359db281fef8cbdd61309547663f2
Geetha1303 <geetha@zelarsoft.com>
2021-06-03 15:41:04 +0530

    jenkinsfile

--- seed.groovy
+++ seed.groovy
@@ -1,11 +1,11 @@
  folder ('CI-Pipelines') {
-   displayname('CI_pipelines')
+   displayName('CI_pipelines')
    description('CI_pipelines')
    }
 pipelinejob{'CI-Pipelines/frontend-ci'} {
  configure { flowdefinition ->
    flowdefinition << delegate.'definition'(class: 'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin: 'workflow-cps') {
-   'scm' {$class: 'hudson.plugins.git.GitSCM'} {
+   'scm'(class: 'hudson.plugins.git.GitSCM',plugin: 'git') {
      'userRemoteConfigs' {
        'hudson.plugins.git.UserRemoteConfig' {
          'url'{'https://github.com/Geetha1303/fontend.git'}
