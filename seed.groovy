99a16a1c409bc454193d6592e386667da118c41b
Geetha1303 <geetha@zelarsoft.com>
2021-06-03 13:32:27 +0530

    jenkinsfile

--- seed.groovy
+++ seed.groovy
@@ -1,6 +1,6 @@
- folder {'CI-Pipelines'} {
-   displayname{'CI pipelines'}
-   description{'CI pipelines'}
+ folder ('CI-Pipelines') {
+   displayname('CI pipelines')
+   description('CI pipelines')
    }
 pipelinejob{'CI-Pipelines/frontend-ci'} {
  configure { Flowdefinition ->
