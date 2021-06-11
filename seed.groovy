16691839c81bf5d150fa92fa3a7730fdd0661103
Geetha1303 <geetha@zelarsoft.com>
2021-06-11 15:49:55 +0530

    jenkinsfile

--- seed.groovy
+++ seed.groovy
@@ -2,23 +2,39 @@
    displayName('CI_pipelines')
    description('CI_pipelines')
    }
-pipelineJob('CI-Pipelines/frontend-ci') {
- configure { flowdefinition ->
-   flowdefinition << delegate.'definition'(class: 'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin: 'workflow-cps') {
-   'scm'(class: 'hudson.plugins.git.GitSCM',plugin: 'git') {
-     'userRemoteConfigs' {
-       'hudson.plugins.git.UserRemoteConfig' {
-         'url'{'https://github.com/Geetha1303/fontend.git'}
-        }
-      }
-      'branches' {
-        'hudson.plugins.git.BranchSpec' {
-          'name' {'*/main'}
+ def component=["cart", "catalogue", "user", "payment", "shipping", "frontend"]
+ def count=(component.size()-1)
+ for (i in 0..count) {
+     def j=component[i]
+     pipelineJob("CI-Pipelines/${j}-ci") {
+         configure { flowdefinition ->
+             flowdefinition / 'properties' << 'org.jenkinsci.plugins.workflow.job.properties.PipelineTriggersJobProperty' {
+                 'triggers' {
+                     'hudson.triggeers.SCMTrigger' {
+                         'spec'('*/2 * * * 1-5')
+                         'ignore PostCommitHooks'(false)
+                     }
+                 }
+             }
+
+             flowdefinition << delegate.'definition'(class: 'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition', plugin: 'workflow-cps') {
+                 'scm'(class: 'hudson.plugins.git.GitSCM', plugin: 'git') {
+                     'userRemoteConfigs' {
+                         'hudson.plugins.git.UserRemoteConfig' {
+                             'url' { 'https://github.com/Geetha1303/' + j + '.git' }
+                             'refspec' { '\'+refs/tags/*\':\'refs/emotes/origin/tags/*\'' }
+                         }
+                     }
+                     'branches' {
+                         'hudson.plugins.git.BranchSpec' {
+                             'name' { '*/tags/*' }
+                         }
+                     }
+                 }
+                 'scriptpath' { 'jenkinsfile' }
+                 'lightweight' { true }
+             }
          }
-      }
-   }
-   'scriptpath'{'jenkinsfile'}
-   'lightweight'{true}
-   }
-  }
-}
+
+     }
+ }
