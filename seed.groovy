5686a778f399e9519db01ead1eed915c3663d81f
Geetha1303 <geetha@zelarsoft.com>
2021-06-21 12:43:36 +0530

    Latest

--- seed.groovy
+++ seed.groovy
@@ -24,12 +24,12 @@
                      'userRemoteConfigs' {
                          'hudson.plugins.git.UserRemoteConfig' {
                              'url'('https://github.com/Geetha1303/'+j+'.git')
-                             'refspec'('\'+refs/tags/*\':\'refs/remotes/origin/tags/*\'')
+                            // 'refspec'('\'+refs/tags/*\':\'refs/remotes/origin/tags/*\'')
                          }
                      }
                      'branches' {
                          'hudson.plugins.git.BranchSpec' {
-                             'name' ('*/tags/*')
+                             'name' ('*/main')
                          }
                      }
                  }
