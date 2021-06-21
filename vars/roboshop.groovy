ec7f3d4abf415388d8f6a48b8dbd62c85751e66e
Geetha1303 <geetha@zelarsoft.com>
2021-06-21 11:55:23 +0530

    vars

---
+++ vars/roboshop.groovy
@@ -0,0 +1,44 @@
+def call(prep params=[:]) {
+    def args=[
+            NEXUS_IP:
+    ]
+    args << params
+    pipeline {
+        agent {
+            label"${args.SLAVE_LABEL}"
+        }
+        environment {
+            COMPONENT="${args.COMPONENT}"
+            NEXUS_IP="${args.NEXUS_IP}"
+            PROJECT_NAME="${args.PROJECT_NAME}"
+            SLAVE_LABEL="${args.SLAVE_LABEL}"
+            APP_TYPE="${args.APP_TYPE}"
+        }
+        stages {
+            stage('code build & install dependencies') {
+                steps{
+                    script{
+                        build=new nexus()
+                        build.code_build("${APP_TYPE}"."${COMPONENT}")
+                    }
+                }
+            }
+            stage('prepare artifacts') {
+                steps{
+                    script{
+                        prepare=new nexus()
+                        prepare.make_artifacts("${APP_TYPE}","${COMPONENT}")
+                    }
+                }
+            }
+            stage('upload artifacts') {
+                steps{
+                    script{
+                        prepare=new nexus()
+                        prepare.nexus{COMPONENT}
+                    }
+                }
+            }
+        }
+    }
+}
