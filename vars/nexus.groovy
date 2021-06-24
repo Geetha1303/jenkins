749518f773e457feb77a00190b8620268f722891
Geetha1303 <geetha@zelarsoft.com>
2021-06-24 15:50:42 +0530

    latest

--- vars/nexus.groovy
+++ vars/nexus.groovy
@@ -14,19 +14,19 @@ def make_artifacts(APP_TYPE, COMPONENT) {
    if(APP_TYPE == "NGINX") {
       command="cd static && zip -r ../${FILENAME}"
       def execute_com=sh(returnStdout: true, script: command)
-      printexecute_com
+      println(execute_com)
    } else if(APP_TYPE == "NODEJS") {
      command="zip -r ${FILENAME} node_modules server.js"
      def execute_com=sh(returnStdout: true, script: command)
-     printexecute_com
+     println(execute_com)
    } else if(APP_TYPE == "JAVA") {
      command="cp target/*.jar ${COMPONENT}.jar && zip -r ${FILENAME} ${COMPONENT}.jar"
      def execute_com=sh(returnStdout: true, script: command)
-     printexecute_com
+     println(execute_com)
    } else if(APP_TYPE == "PYTHON") {
      command="zip -r ${FILENAME} payment.ini payment.py rabbitmq.py requirements.txt"
      def execute_com=sh(returnStdout: true, script: command)
-     printexecute_com
+     println(execute_com)
    }
 }
 
@@ -34,10 +34,10 @@ def code_build(APP_TYPE, COMPONENT) {
   if(APP_TYPE == "NODEJS") {
      command = "npm install"
      def execute_com=sh(returnStdout: true, script: command)
-     printexecute_com
+     println(execute_com)
   }  else if(APP_TYPE == "JAVA") {
      command = "mvn clean package"
      def execute_com=sh(returnStdout: true, script: command)
-     printexecute_com
+     println(execute_com)
   }
 }
